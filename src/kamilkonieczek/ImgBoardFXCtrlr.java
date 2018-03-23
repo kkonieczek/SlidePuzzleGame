c6f9ae99dd1098ff265673f2bfe01c4c574c12a1
Kamil Konieczek <k.konieczek@gmail.com>
2018-03-23 22:49:26 +0100

    Add image position property and bind to fxml images

--- src/kamilkonieczek/ImgBoardFXCtrlr.java
+++ src/kamilkonieczek/ImgBoardFXCtrlr.java
@@ -1,10 +1,19 @@
 package kamilkonieczek;
 
+import javafx.beans.property.DoubleProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 
+import java.util.ArrayList;
+
 public class ImgBoardFXCtrlr {
+
+    ImgBoardViewModel imgBoardViewModel = new ImgBoardViewModel();
+
+    //ArrayList<DoubleProperty> imgPropArray = new  ArrayList<DoubleProperty>();
+
+    
 
     @FXML
     public ImageView img11;
@@ -19,10 +28,65 @@ public class ImgBoardFXCtrlr {
 
     @FXML
     void initialize(){
-        img11.setX(0);
-        img11.setY(0);
-        img12.setX(0);
-        img12.setY(0);
+        img11.setLayoutX(0);
+        img12.setLayoutX(0);
+        img13.setLayoutX(0);
+        img21.setLayoutX(0);
+        img22.setLayoutX(0);
+        img23.setLayoutX(0);
+        img31.setLayoutX(0);
+        img32.setLayoutX(0);
+        img33.setLayoutX(0);
+
+        img11.setLayoutY(0);
+        img12.setLayoutY(0);
+        img13.setLayoutY(0);
+        img21.setLayoutY(0);
+        img22.setLayoutY(0);
+        img23.setLayoutY(0);
+        img31.setLayoutY(0);
+        img32.setLayoutY(0);
+        img33.setLayoutY(0);
+
+        imgBoardViewModel.setImg11X(25);
+        imgBoardViewModel.setImg12X(175);
+        imgBoardViewModel.setImg13X(325);
+        imgBoardViewModel.setImg21X(25);
+        imgBoardViewModel.setImg22X(175);
+        imgBoardViewModel.setImg23X(325);
+        imgBoardViewModel.setImg31X(25);
+        imgBoardViewModel.setImg32X(175);
+        imgBoardViewModel.setImg33X(325);
+
+        imgBoardViewModel.setImg11Y(25);
+        imgBoardViewModel.setImg12Y(25);
+        imgBoardViewModel.setImg13Y(25);
+        imgBoardViewModel.setImg21Y(175);
+        imgBoardViewModel.setImg22Y(175);
+        imgBoardViewModel.setImg23Y(175);
+        imgBoardViewModel.setImg31Y(325);
+        imgBoardViewModel.setImg32Y(325);
+        imgBoardViewModel.setImg33Y(325);
+
+        img11.xProperty().bindBidirectional(imgBoardViewModel.img11XProperty());
+        img12.xProperty().bindBidirectional(imgBoardViewModel.img12XProperty());
+        img13.xProperty().bindBidirectional(imgBoardViewModel.img13XProperty());
+        img21.xProperty().bindBidirectional(imgBoardViewModel.img21XProperty());
+        img22.xProperty().bindBidirectional(imgBoardViewModel.img22XProperty());
+        img23.xProperty().bindBidirectional(imgBoardViewModel.img23XProperty());
+        img31.xProperty().bindBidirectional(imgBoardViewModel.img31XProperty());
+        img32.xProperty().bindBidirectional(imgBoardViewModel.img32XProperty());
+        img33.xProperty().bindBidirectional(imgBoardViewModel.img33XProperty());
+
+        img11.yProperty().bindBidirectional(imgBoardViewModel.img11YProperty());
+        img12.yProperty().bindBidirectional(imgBoardViewModel.img12YProperty());
+        img13.yProperty().bindBidirectional(imgBoardViewModel.img13YProperty());
+        img21.yProperty().bindBidirectional(imgBoardViewModel.img21YProperty());
+        img22.yProperty().bindBidirectional(imgBoardViewModel.img22YProperty());
+        img23.yProperty().bindBidirectional(imgBoardViewModel.img23YProperty());
+        img31.yProperty().bindBidirectional(imgBoardViewModel.img31YProperty());
+        img32.yProperty().bindBidirectional(imgBoardViewModel.img32YProperty());
+        img33.yProperty().bindBidirectional(imgBoardViewModel.img33YProperty());
     }
 
     @FXML
---
+++ src/kamilkonieczek/ImgBoardViewModel.java
@@ -0,0 +1,243 @@
+package kamilkonieczek;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+
+public class ImgBoardViewModel {
+
+    private DoubleProperty img11X = new SimpleDoubleProperty();
+    private DoubleProperty img12X = new SimpleDoubleProperty();
+    private DoubleProperty img13X = new SimpleDoubleProperty();
+    private DoubleProperty img21X = new SimpleDoubleProperty();
+    private DoubleProperty img22X = new SimpleDoubleProperty();
+    private DoubleProperty img23X = new SimpleDoubleProperty();
+    private DoubleProperty img31X = new SimpleDoubleProperty();
+    private DoubleProperty img32X = new SimpleDoubleProperty();
+    private DoubleProperty img33X = new SimpleDoubleProperty();
+
+    public double getImg11X() {
+        return img11X.get();
+    }
+
+    public DoubleProperty img11XProperty() {
+        return img11X;
+    }
+
+    public void setImg11X(double img11X) {
+        this.img11X.set(img11X);
+    }
+
+    public double getImg12X() {
+        return img12X.get();
+    }
+
+    public DoubleProperty img12XProperty() {
+        return img12X;
+    }
+
+    public void setImg12X(double img12X) {
+        this.img12X.set(img12X);
+    }
+
+    public double getImg13X() {
+        return img13X.get();
+    }
+
+    public DoubleProperty img13XProperty() {
+        return img13X;
+    }
+
+    public void setImg13X(double img13X) {
+        this.img13X.set(img13X);
+    }
+
+    public double getImg21X() {
+        return img21X.get();
+    }
+
+    public DoubleProperty img21XProperty() {
+        return img21X;
+    }
+
+    public void setImg21X(double img21X) {
+        this.img21X.set(img21X);
+    }
+
+    public double getImg22X() {
+        return img22X.get();
+    }
+
+    public DoubleProperty img22XProperty() {
+        return img22X;
+    }
+
+    public void setImg22X(double img22X) {
+        this.img22X.set(img22X);
+    }
+
+    public double getImg23X() {
+        return img23X.get();
+    }
+
+    public DoubleProperty img23XProperty() {
+        return img23X;
+    }
+
+    public void setImg23X(double img23X) {
+        this.img23X.set(img23X);
+    }
+
+    public double getImg31X() {
+        return img31X.get();
+    }
+
+    public DoubleProperty img31XProperty() {
+        return img31X;
+    }
+
+    public void setImg31X(double img31X) {
+        this.img31X.set(img31X);
+    }
+
+    public double getImg32X() {
+        return img32X.get();
+    }
+
+    public DoubleProperty img32XProperty() {
+        return img32X;
+    }
+
+    public void setImg32X(double img32X) {
+        this.img32X.set(img32X);
+    }
+
+    public double getImg33X() {
+        return img33X.get();
+    }
+
+    public DoubleProperty img33XProperty() {
+        return img33X;
+    }
+
+    public void setImg33X(double img33X) {
+        this.img33X.set(img33X);
+    }
+
+    public double getImg11Y() {
+        return img11Y.get();
+    }
+
+    public DoubleProperty img11YProperty() {
+        return img11Y;
+    }
+
+    public void setImg11Y(double img11Y) {
+        this.img11Y.set(img11Y);
+    }
+
+    public double getImg12Y() {
+        return img12Y.get();
+    }
+
+    public DoubleProperty img12YProperty() {
+        return img12Y;
+    }
+
+    public void setImg12Y(double img12Y) {
+        this.img12Y.set(img12Y);
+    }
+
+    public double getImg13Y() {
+        return img13Y.get();
+    }
+
+    public DoubleProperty img13YProperty() {
+        return img13Y;
+    }
+
+    public void setImg13Y(double img13Y) {
+        this.img13Y.set(img13Y);
+    }
+
+    public double getImg21Y() {
+        return img21Y.get();
+    }
+
+    public DoubleProperty img21YProperty() {
+        return img21Y;
+    }
+
+    public void setImg21Y(double img21Y) {
+        this.img21Y.set(img21Y);
+    }
+
+    public double getImg22Y() {
+        return img22Y.get();
+    }
+
+    public DoubleProperty img22YProperty() {
+        return img22Y;
+    }
+
+    public void setImg22Y(double img22Y) {
+        this.img22Y.set(img22Y);
+    }
+
+    public double getImg23Y() {
+        return img23Y.get();
+    }
+
+    public DoubleProperty img23YProperty() {
+        return img23Y;
+    }
+
+    public void setImg23Y(double img23Y) {
+        this.img23Y.set(img23Y);
+    }
+
+    public double getImg31Y() {
+        return img31Y.get();
+    }
+
+    public DoubleProperty img31YProperty() {
+        return img31Y;
+    }
+
+    public void setImg31Y(double img31Y) {
+        this.img31Y.set(img31Y);
+    }
+
+    public double getImg32Y() {
+        return img32Y.get();
+    }
+
+    public DoubleProperty img32YProperty() {
+        return img32Y;
+    }
+
+    public void setImg32Y(double img32Y) {
+        this.img32Y.set(img32Y);
+    }
+
+    public double getImg33Y() {
+        return img33Y.get();
+    }
+
+    public DoubleProperty img33YProperty() {
+        return img33Y;
+    }
+
+    public void setImg33Y(double img33Y) {
+        this.img33Y.set(img33Y);
+    }
+
+    private DoubleProperty img11Y = new SimpleDoubleProperty();
+    private DoubleProperty img12Y = new SimpleDoubleProperty();
+    private DoubleProperty img13Y = new SimpleDoubleProperty();
+    private DoubleProperty img21Y = new SimpleDoubleProperty();
+    private DoubleProperty img22Y = new SimpleDoubleProperty();
+    private DoubleProperty img23Y = new SimpleDoubleProperty();
+    private DoubleProperty img31Y = new SimpleDoubleProperty();
+    private DoubleProperty img32Y = new SimpleDoubleProperty();
+    private DoubleProperty img33Y = new SimpleDoubleProperty();
+}
