ce7242c708cd48f4720bf4605337649de1595b73
Kamil Konieczek <k.konieczek@gmail.com>
2018-04-23 20:53:18 +0200

    Reordered code

--- src/kamilkonieczek/TilesPosArray.java
+++ src/kamilkonieczek/TilesPosArray.java
@@ -2,15 +2,16 @@ package kamilkonieczek;
 
 public class TilesPosArray {
 
-    private double tilePosX[] = new double[9];
-    private double tilePosY[] = new double[9];
-
-    public double[] getTilePosX() {
-        return tilePosX;
-    }
-
-    public double[] getTilePosY() {
-        return tilePosY;
+    private double tileHeight;
+    private double tileWidth;
+    private double heightOffset;
+    private double widthOffset;
+    
+    TilesPosArray(double tileWidth, double tileHeight, double heightOffset, double widthOffset){
+        this.tileWidth = tileWidth;
+        this.tileHeight = tileHeight;
+        this.widthOffset = widthOffset;
+        this.heightOffset = heightOffset;
     }
 
     private int tileOrder[] = new int[9];
@@ -19,10 +20,8 @@ public class TilesPosArray {
         this.tileOrder = tileOrder;
     }
 
-    private double tileHeight;
-    private double tileWidth;
-    private double heightOffset;
-    private double widthOffset;
+    private double tilePosX[] = new double[9]; // array of X coordinate of tiles position
+    private double tilePosY[] = new double[9]; // array of Y coordinate of tiles position
 
     public void calcTilePos(){
         for(int i =0; i<9; i++){
@@ -31,11 +30,12 @@ public class TilesPosArray {
         }
     }
 
-    TilesPosArray(double tileWidth, double tileHeight, double heightOffset, double widthOffset){
-        this.tileWidth = tileWidth;
-        this.tileHeight = tileHeight;
-        this.widthOffset = widthOffset;
-        this.heightOffset = heightOffset;
+    public double[] getTilePosX() {
+        return tilePosX;
+    }
+
+    public double[] getTilePosY() {
+        return tilePosY;
     }
 
 }
