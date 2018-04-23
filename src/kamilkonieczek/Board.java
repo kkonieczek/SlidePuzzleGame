6326ce4b8ad1ef544b10a0bf7e77f15e5ff67403
Kamil Konieczek <k.konieczek@gmail.com>
2018-04-23 20:21:14 +0200

    Refactoring: Atribute tilesOrder removed from class Board

--- src/kamilkonieczek/Board.java
+++ src/kamilkonieczek/Board.java
@@ -12,10 +12,9 @@ public class Board {
 
     public ArrayList<Tile> tiles = new ArrayList<Tile>();	// array of tiles
 
-    private int[] tilesOrder = {0,1,2,3,4,5,6,7,8};
-
 	public int[] getTilesOrder() {
 		updateTileMap();
+        int[] tilesOrder = new int[9];
 		for(int i =0; i<9; i++){
 			tilesOrder[i]=tileMap.get(i);
 		}
